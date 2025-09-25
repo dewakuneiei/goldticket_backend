b18a09efd1a57b160c31cbc0cb4d736b8058159f
dewakuneiei <dewakuneiei@gmail.com>
2025-09-25 18:31:05 +0700

    update allowedOrigins

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ const app = express();
 // =============================================================================
 const allowedOrigins = [
   'https://lacoupong.vercel.app',
-  'http://localhost:5501',
-  'http://127.0.0.1:5501',
-  'http://127.0.0.1:5500'
+  'https://lacoupong-website-admin.vercel.app'
 ];
 
 const corsOptions = {
